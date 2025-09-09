94249a03ba64313b99a4a5a07e0c953008d344a4
erlmek <erlm@ek.dk>
2025-09-09 09:21:48 +0200

    Initial commit

---
+++ crazy.js
@@ -0,0 +1,26 @@
+function out(x) {
+    console.log(x)
+}
+
+function vis2(x, y) {
+    console.log("x=" + x, " y=" + y);
+}
+
+let a = 5; let b = 7;
+console.log(a+b)
+
+let arr = [1, 2, 5, "hej", 5.5, 5>"hej"]
+
+arr.forEach(vis2)
+
+arr.push(4)
+arr.push("mere til array")
+
+console.log(30 - a>b)
+console.log(30 - (a>b))
+console.log(30 - (b>a))
+console.log(!(b>a))
+
+console.log(typeof out)
+console.log(typeof arr)
+
